c7e635ec84d1a2da36c1bb00be50117347e95202
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-28 05:46:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_number = nil
 name_hash.each do |name,number|
- if lowest_number >nil
+ if number > nil
   answer = name
   lowest_number = number
  elsif lowest_number > number
