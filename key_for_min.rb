363c09b0a957dd4b97cd6379a3a21b11e800887e
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-28 05:24:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_number = nil
 
+  name_hash.each do |key,value|
+ if value == nil
+  return value
+  lowest_number = value
+elsif lowest_number > value
+   answer = key
+   lowest_number = value
+ end
+end
+answer
 end
